96faf5210f52bbc05973cbe443af0c8b2779c187
Evandro Rodrigues <evandro.rodriguespj@gmail.com>
2024-11-17 14:59:58 -0300

    chapter-13

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -51,7 +51,7 @@ func (app *application) userSignupPost(w http.ResponseWriter, r *http.Request) {
 
 	form.CheckField(validator.NotBlank(form.Name), "name", "This field cannot be blank")
 	form.CheckField(validator.NotBlank(form.Email), "email", "This field cannot be blank")
-	form.CheckField(validator.Matches(form.Email, validator.EmailRX), "email", "This field must be a valid e-mail address")
+	form.CheckField(validator.Matches(form.Email, validator.EmailRX), "email", "This field must be a valid email address")
 	form.CheckField(validator.NotBlank(form.Password), "password", "This field cannot be blank")
 	form.CheckField(validator.MinChars(form.Password, 8), "password", "This field must be at least 8 characters long")
 
